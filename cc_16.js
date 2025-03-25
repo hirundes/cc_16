930244209f8c82dc722cb93ea1c2edb8c729a2c6
HIRUN <hirundesilva97@gmail.com>
2025-03-25 17:11:14 -0400

    CHANGE: Formatting and Added comments

--- cc_16.js
+++ cc_16.js
@@ -1,12 +1,11 @@
 //Task 2
 function fetchProductsThen() {
-    fetch('https://www.course-api.com/javascript-store-products')
+    fetch('https://www.course-api.com/javascript-store-products') //fetch to get product data from link
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-        const data = response.json(); 
-        // console.log(data); 
+        const data = response.json(); //logging product names to console
         return data
     })
     .then(data => {
@@ -14,57 +13,51 @@ function fetchProductsThen() {
             console.log(product.fields.name);
         });
     })
-    .catch(error => {
+    .catch(error => { //.catch() to log any errors
         console.error('There was a problem with the fetch operation:', error);
     });
 }
 
-fetchProductsThen();
-
-
 //Task 3
-async function fetchProductsAsync() {
+async function fetchProductsAsync() { //uses async and await and try/catch to fetch product data from link
     try {
         const response = await fetch('https://www.course-api.com/javascript-store-products');
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         const products = await response.json();
-        displayProducts(products.slice(0,5)); 
-        
-    } catch (error) {
-        handleError(error); 
+        displayProducts(products.slice(0,5)); //calling helper function and also loops through first 5 products
+                                             
+    } catch (error) { 
+        handleError(error); //if error occurs it is passed here
     }
 }
 
-fetchProductsAsync(); 
-
-
 //Task 4
 function displayProducts(products) {
-    const container = document.getElementById("product-container");
+    const container = document.getElementById("product-container"); //selection of product-container
     container.innerHTML = ""; 
 
     products.forEach(product => {
         const productDisplay = document.createElement("div");
         productDisplay.classList.add("product");
 
-        productDisplay.innerHTML = `
+        //HTML elements created to show name, price, and image
+        productDisplay.innerHTML = ` 
             <h2>${product.fields.name}</h2>
             <p>Price: $${product.fields.price}</p>
             <img src="${product.fields.image[0].url}">
         `;
 
-        container.appendChild(productDisplay);
+        container.appendChild(productDisplay); //appends them to the container
     });
 } 
 
-
 //Task 5
-function handleError(error) {
+function handleError(error) { //function handleError(error) which logs error message
     console.error('There was a problem fetching products:', error);
 }; 
 
 //Task 6
-fetchProductsThen();
+fetchProductsThen(); //calling both
 fetchProductsAsync();
